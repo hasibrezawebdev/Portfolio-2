505d11504755b2819c6b8a1b69bc8355e62493a4
Md Hasib Reza <120497592+hasibrezawebdev@users.noreply.github.com>
2023-03-17 16:46:10 +0530

    adding files

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let nav = document.querySelector("nav");
+let navUl= document.querySelector("#navUl");
+let home = document.querySelector("#hm");
+let about = document.querySelector("#abt");
+let skills = document.querySelector("#skl");
+let menuIcon = document.querySelector("#menuIcon");
+let mobileNav = document.querySelector("#mobileNav").style;
+
+function navBarToggle(){
+    nav.classList.toggle("toggleClass");
+    navUl.classList.toggle("toggleClass");
+    if (mobileNav.display === "flex") {
+      mobileNav.display = "none";
+    } else {
+      mobileNav.display = "flex";
+    }
+}
+function removeClass(){
+    nav.classList.remove("toggleClass");
+    navUl.classList.remove("toggleClass");
+    if (mobileNav.display === "flex") {
+      mobileNav.display = "none";
+    }
+}
+
+home.addEventListener("click", removeClass);
+menuIcon.addEventListener("click", navBarToggle);
+about.addEventListener("click", removeClass);
+skills.addEventListener("click", removeClass);
